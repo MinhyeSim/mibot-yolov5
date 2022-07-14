fc57e2201b45fc4c72d0ab257491c62ff069ad69
MinhyeSim <smine0032@gmail.com>
2022-07-14 16:14:33 +0900

    first commit

---
+++ app/execute.sh
@@ -0,0 +1,3 @@
+python arg_example.py --data 1
+python arg_example.py --data 3 --ls 0.4
+python arg_example.py --l2 1e-4 --cutmix 'y'
---
+++ app/module_example.py
@@ -0,0 +1,9 @@
+def linear_regression(data):
+    print("Linear Regression")
+
+def gaussian_mixture(data):
+    print("Gaussian Mixture")
+
+def random_forest(data):
+    print("Random Forest")       
+    
