0356830ca21395960b72e37ad9e633c64da71592
MinhyeSim <smine0032@gmail.com>
2022-07-21 19:43:13 +0900

    mibot stt

--- stt_test/stt.py
+++ stt_test/stt.py
@@ -15,14 +15,22 @@ class Solution:
         pass
     
     def save_file(self):
-        tts = gTTS(text = "신나는 곡 들려드릴게요", lang="ko")
-        tts.save(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\music.mp3")
+        tts = gTTS(text = "다음에 또 만나요", lang="ko")
+        tts.save(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\bye.mp3")
 
     def speak(self):       
         return playsound.playsound(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\mibot.mp3")
 
     def speak2(self):       
         return playsound.playsound(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\music.mp3")
+
+    def speak3(self):       
+        return playsound.playsound(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\today.mp3")
+
+    def speak4(self):       
+        return playsound.playsound(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\bye.mp3")
+
+    
 
     def get_audio(self):
         r = sr.Recognizer() #음성 인식
@@ -31,7 +39,7 @@ class Solution:
             said = "" # 말하기 -> 한국말로 음성 인식 되게 하기 
                  
             try:
-                said = r.recognize_google(audio)
+                said = r.recognize_google(audio, language='ko-KR')
                 print(said)
             except Exception as e:
                 print("Exception: " + str(e))
@@ -40,12 +48,17 @@ class Solution:
     def stt(self):
         self.speak()
         self.text = self.get_audio()
-
-        if "hello" in self.text:
-            self.speak()
-        elif "boring" in self.text:
-            self.speak2()
-
+    
+        while 1:
+            self.text = self.get_audio()
+            if "안녕" in self.text:
+                self.speak3()
+            elif "힘들어" in self.text:
+                self.speak2()                
+            elif "잘가" in self.text:
+                self.speak4()
+                break
+    
 if __name__ == "__main__": 
     #Solution().save_file()
     #Solution().speak()
