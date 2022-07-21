4639439b0461305004cae4457ffa3bc4584b23d5
MinhyeSim <smine0032@gmail.com>
2022-07-21 16:24:07 +0900

    2022-07-21 16:23 stt

--- stt_test/stt.py
+++ stt_test/stt.py
@@ -6,30 +6,52 @@ import time
 import playsound
 from yaml import safe_dump_all
 
-def speak(text):
-    tts = gTTS(text=text, lang="ko")
-    tts.save(r"stt_test\voice.mp3")
-    playsound(r"stt_test\voice.mp3")
+class Solution:
 
-def get_audio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        said = " "
-            
-        try:
-            said = r.recognize_google(audio)
-            print(said)
-        except Exception as e:
-            print("Exception: " + str(e))
-    return said 
+    def __init__(self) :
+        #self.text = "말씀하세요"
+        #self.tts = gTTS(text = self.text, lang="ko")
+        #self.tts.save(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\voice.mp3")
+        pass
+    
+    def save_file(self):
+        tts = gTTS(text = "신나는 곡 들려드릴게요", lang="ko")
+        tts.save(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\music.mp3")
 
-#speak("하이하이")
-text = get_audio()
+    def speak(self):       
+        return playsound.playsound(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\mibot.mp3")
 
-if "안녕" in text:
-    speak("반가워요, 저는 당신의 미봇이예요")
-elif "심심해" in text:
-    speak("정신차려요. 이 각박한 세상속에서")
+    def speak2(self):       
+        return playsound.playsound(r"C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test\music.mp3")
+
+    def get_audio(self):
+        r = sr.Recognizer() #음성 인식
+        with sr.Microphone() as source: #마이크로폰을 소스로 받아들여 
+            audio = r.listen(source) # 오디오로 들어간 뒤
+            said = "" # 말하기 -> 한국말로 음성 인식 되게 하기 
+                 
+            try:
+                said = r.recognize_google(audio)
+                print(said)
+            except Exception as e:
+                print("Exception: " + str(e))
+        return said 
+
+    def stt(self):
+        self.speak()
+        self.text = self.get_audio()
+
+        if "hello" in self.text:
+            self.speak()
+        elif "boring" in self.text:
+            self.speak2()
+
+if __name__ == "__main__": 
+    #Solution().save_file()
+    #Solution().speak()
+    #Solution().get_audio()
+    Solution().stt()
+
+
 
 
--- tts_test/tts.py
+++ tts_test/tts.py
@@ -5,7 +5,7 @@ class Solution:
 
     def __init__(self):
     
-        self.text = "진영아 안녕"
+        self.text = "안녕하세요 당신의 미봇이에요"
         self.tts = gTTS(text = self.text, lang="ko")
         self.tts.save(r"C:\Users\bitcamp\mibot\mibot-yolov5\yolov5\tts_test\hi.mp3")
     
