ce76fccdabb4794bba93fdb91fa2931b032f58fa
MinhyeSim <smine0032@gmail.com>
2022-07-26 12:16:14 +0900

    12:16 stt_user happy ver.

--- stt_test/stt.py
+++ stt_test/stt.py
@@ -14,8 +14,8 @@ class Solution:
         self.path = "C:/Users/bitcamp/mibot/mibot-yolov5/yolov5/stt_test"
     
     def save_file(self):
-        tts = gTTS(text = "슬픈 기분을 위해 비투비의 괜찮아요를 들려 드릴게요", lang="ko")
-        tts.save(f"{self.path}/play.mp3") 
+        tts = gTTS(text = "신나는 여행이 될 수 있도록 딱 맞는 음악을 추천해 드릴까요?  ", lang="ko")
+        tts.save(f"{self.path}/recommend2.mp3") 
 
     def mibot_speak(self):       
         return playsound.playsound(f"{self.path}/mibot.mp3") 
@@ -26,14 +26,14 @@ class Solution:
     def music_speak(self):       
         return playsound.playsound(f"{self.path}/kobert.mp3") 
 
-    def sad_speak1(self):       
-        return playsound.playsound(f"{self.path}/cheerup.mp3") 
+    def happy_speak1(self):       
+        return playsound.playsound(f"{self.path}/jeju.mp3") 
     
-    def sad_speak2(self):       
-        return playsound.playsound(f"{self.path}/recommend.mp3")
+    def happy_speak2(self):       
+        return playsound.playsound(f"{self.path}/recommend2.mp3")
     
-    def sad_speak3(self):       
-        return playsound.playsound(f"{self.path}/play.mp3") 
+    def happy_speak3(self):       
+        return playsound.playsound(f"{self.path}/play2.mp3") 
     
 
     
@@ -58,6 +58,7 @@ class Solution:
             self.text = self.get_audio()
             if "대화 종료" in self.text:
                 self.bye_speak()
+                break
             elif "음악 추천" in self.text:
                 self.music_speak()                
             elif "면접" in self.text:
@@ -66,17 +67,40 @@ class Solution:
                 self.sad_speak2()
             elif "슬퍼" in self.text:
                 self.sad_speak3()   
+
+    def user_happy(self):
+        self.mibot_speak()
+           
+        while 1:
+            self.text = self.get_audio()
+            if "대화 종료" in self.text:
+                self.bye_speak()
                 break
+            elif "음악 추천" in self.text:
+                self.music_speak()                
+            elif "제주도 여행" in self.text:
+                self.happy_speak1()
+            elif "기대돼" in self.text:
+                self.happy_speak2()
+            elif "행복해" in self.text:
+                self.happy_speak3()   
+
+                
     
 if __name__ == "__main__": 
-    Solution().save_file()
+    #Solution().save_file()
     #Solution().mibot_speak()
     #Solution().bye_speak()
     #Solution().music_speak()
     #Solution().sad_speak1()
     #Solution().sad_speak2()
     #Solution().sad_speak3()
-    Solution().user_sad()
+    #Solution().happy_speak1()
+    #Solution().happy_speak2()
+    #Solution().happy_speak3()
+    #Solution().user_sad()
+    Solution().user_happy()
+
 
 
 
